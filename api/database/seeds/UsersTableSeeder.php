6c9907509dc4528421b3221d40d264e769078ffe
Talles Gazel <tjgazel@gmail.com>
2017-10-09 11:29:02 -0300

    Vídeo 04

---
+++ api/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+            'name' => 'Talles Gazel',
+            'email' => 'admin@gmail.com'
+        ]);
+    }
+}
