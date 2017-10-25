1507cfd4f29f7d0d325535488edb176341df28a1
Talles Gazel <tjgazel@gmail.com>
2017-10-25 13:04:54 -0200

    video #17

--- web/src/app/admin/admin-header/admin-header.component.ts
+++ web/src/app/admin/admin-header/admin-header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../auth/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  logout(e){
+    e.preventDefault();
+    this.auth.logout();
   }
 
 }
--- web/src/app/admin/admin-left-side/admin-left-side.component.ts
+++ web/src/app/admin/admin-left-side/admin-left-side.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../auth/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLeftSideComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
